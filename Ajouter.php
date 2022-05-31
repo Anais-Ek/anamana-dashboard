5778bf3826e3b4a920f91a428113e342f08ccf81
Anais-Ek <anaiselisa77@gmail.com>
2022-05-31 13:41:22 +0200

    test

--- Ajouter.php
+++ Ajouter.php
@@ -1,4 +1,50 @@
-<?php require_once('connexion.php');?>
+<?php 
+	require_once('connexion.php');
+
+	if(isset($_POST['btadd']))
+	{
+		$cat = $_POST['txtCategorie'];
+		$couleur=$_POST['txtCouleur'];
+		$prix=$_POST['txtP'];
+				
+  		$image = $_FILES['txtphoto']['tmp_name'];
+		//$image_text = mysqli_real_escape_string($cnloccar, $_POST['txtphoto']);
+	  	$target = "images/".$_FILES['txtphoto']['name'];
+		//$target = "images/".basename($image);
+		if (move_uploaded_file($image,$target)) {
+  			$msg = "Image téléchargée avec succès";
+  		}else{
+  			$msg = "Impossible de télécharger l'image";
+		  }
+
+		$imagep = $_FILES['txtplan']['tmp_name'];
+		//$image_text = mysqli_real_escape_string($cnloccar, $_POST['txtphoto']);
+		$targetp = "images/".$_FILES['txtplan']['name'];
+		//$target = "images/".basename($image);
+		if (move_uploaded_file($imagep,$targetp)) {
+			$msg = "Image téléchargée avec succès";
+		}else{
+			$msg = "Impossible de télécharger l'image";
+		}
+		  
+		$cnanamana=new MySQLi("51.159.27.252:50992","anamana","Fenelon2022","anamana");
+		if(!$cnanamana->connect_error)
+		{
+			$query = "INSERT INTO produits (CATEGORIE,COULEUR,PRIX,PHOTO,PLAN) VALUES ('$cat','$couleur','$prix', '$target', '$targetp')";
+			$resultat=mysqli_query($cnanamana,$query);
+			if($resultat)
+			{
+				echo "Insertion des données validés";
+			}else{
+				echo "Echec d'insertion des données !<BR/>";
+				echo ($cnanamana->error)
+			}
+		}
+		else die('erreur connexion base de données..')
+  	
+  	}
+
+?>
 <!doctype html>
 <html>
 
@@ -113,46 +159,7 @@
                 
                 <label style="text-align: center;color: #360001;">
                 	
-                	<?php
-	if(isset($_POST['btadd']))
-	{
-		$cat=$_POST['txtCategorie'];
-		$couleur=$_POST['txtCouleur'];
-		$prix=$_POST['txtP'];
-				
-  		$image = $_FILES['txtphoto']['tmp_name'];
-		//$image_text = mysqli_real_escape_string($cnloccar, $_POST['txtphoto']);
-	  	$target = "images/".$_FILES['txtphoto']['name'];
-		//$target = "images/".basename($image);
-		if (move_uploaded_file($image,$target)) {
-  			$msg = "Image téléchargée avec succès";
-  		}else{
-  			$msg = "Impossible de télécharger l'image";
-		  }
 
-		$imagep = $_FILES['txtplan']['tmp_name'];
-		//$image_text = mysqli_real_escape_string($cnloccar, $_POST['txtphoto']);
-		$targetp = "images/".$_FILES['txtplan']['name'];
-		//$target = "images/".basename($image);
-		if (move_uploaded_file($imagep,$targetp)) {
-			$msg = "Image téléchargée avec succès";
-		}else{
-			$msg = "Impossible de télécharger l'image";
-		}
-		  
-  	$sql = "INSERT INTO produits (CATEGORIE,COULEUR,PRIX,PHOTO,PLAN) VALUES ('$cat','$couleur','$prix', '$target', '$targetp')";
-		$resultat=mysqli_query($cnanamana,$sql);
-
-if($resultat)
-{
-	echo "Insertion des données validés";
-}else{
-	echo "Echec d'insertion des données !";
-}
-  	
-  }
-  
-	?>
                        	
 </label>
 	</form>
